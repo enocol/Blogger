e0f9790cfcbdecccb814fc163690051f17e480bd
enocol <eno_col@yahoo.com>
2024-01-11 08:01:26 +0000

    Edit users migration files

--- db/migrate/20240111062838_create_users.rb
+++ db/migrate/20240111062838_create_users.rb
@@ -3,15 +3,12 @@
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
-      t.string :title
-      t.text :text
-      t.integer :comments_counter
-      t.string :likes_counter
-      t.bigint :author_id
+      t.string :name
+      t.string :bio
+      t.string :photo
+      t.string :posts_counter
 
       t.timestamps
-      t.index :author_id
-      t.foreign_key :users, column: :author_id, primary_key: :id, on_delete: :cascade
     end
   end
 end
