ff4ee7dfe8288c03c6979c0146584db61f45b459
enocol <eno_col@yahoo.com>
2024-01-13 20:11:58 +0000

    Add validations for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :like, dependent: :destroy
   validates :name, presence: true
+  
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   def recent_posts(limit = 3)
