61aad52ec301410208ce76f892b32d118fc1b7f0
enocol <eno_col@yahoo.com>
2024-01-13 17:52:14 +0000

    schema.rb updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_11_163658) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_13_164840) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
     t.text "text"
-    t.bigint "user_id"
-    t.bigint "post_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
@@ -25,8 +25,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_11_163658) do
   end
 
   create_table "likes", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "post_id"
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_likes_on_post_id"
@@ -34,36 +34,30 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_11_163658) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string "title"
+    t.bigint "author_id", null: false
+    t.string "title", null: false
     t.text "text"
-    t.integer "comments_counter"
-    t.integer "likes_counter"
-    t.bigint "author_id"
+    t.integer "comments_counter", default: 0, null: false
+    t.integer "likes_counter", default: 0, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "teachers", force: :cascade do |t|
-    t.string "name"
-    t.string "bio"
-    t.string "photo"
-    t.string "posts_counter"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "contact"
+    t.integer "posts_counter", default: 0
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.text "bio"
-    t.integer "photo"
-    t.string "posts_counter"
+    t.string "photo"
+    t.integer "posts_counter", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "role", default: 1
   end
 
-  add_foreign_key "comments", "posts", on_delete: :cascade
-  add_foreign_key "comments", "users", on_delete: :cascade
-  add_foreign_key "likes", "posts", on_delete: :cascade
-  add_foreign_key "likes", "users", on_delete: :cascade
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
