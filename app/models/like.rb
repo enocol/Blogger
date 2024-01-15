9b317bc7481991d30d784729bc820d769c7779a0
enocol <eno_col@yahoo.com>
2024-01-15 22:43:24 +0000

    Edit like.rb and post.rb

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,12 +1,20 @@
 class Like < ApplicationRecord
   belongs_to :user, foreign_key: 'user_id'
-  belongs_to :post, foreign_key: 'post_id'
+  belongs_to :post, foreign_key: 'post_id', counter_cache: :likes_counter
   after_save :update_post_likes_counter
   after_destroy :update_post_likes_counter
 
   private
 
+  # def update_post_likes_counter
+  #   post.increment(:likes_counter)
+  # end
+
   def update_post_likes_counter
-    post.increment(:likes_counter)
+    # Check if the associated post exists
+    if post.present?
+      # Increment the likes_counter on the associated post
+      post.update_column(:likes_counter, post.likes.count)
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, dependent: :destroy
-  has_many :likes, dependent: :destroy
+  # has_many :likes, dependent: :destroy
+  has_many :likes, foreign_key: 'post_id', dependent: :destroy
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :posts_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
@@ -15,6 +16,9 @@ class Post < ApplicationRecord
   private
 
   def update_posts_counter
-    post.increment!(:posts_counter)
+    # post.increment!(:posts_counter)
+    increment!(:posts_counter)
   end
+
+
 end
