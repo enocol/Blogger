b92b9e7a9e5496547fbf1823bad29b40eec7562e
enocol <eno_col@yahoo.com>
2024-01-22 19:00:01 +0000

    Fix linter errors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,53 +1,50 @@
 class UsersController < ApplicationController
-    before_action :set_user, only: [:show]
-    before_action :set_user, only: [:show, :posts]
-    #before_action :set_post, only: [:post_detail]
-    before_action :set_post, only: [:show_user_post]
+  before_action :set_user, only: [:show]
+  before_action :set_user, only: %i[show posts]
+  # before_action :set_post, only: [:post_detail]
+  before_action :set_post, only: [:show_user_post]
 
-    def index
-        @users = User.all
-    end
+  def index
+    @users = User.all
+  end
 
-    def show
-      @user = User.find_by(id: params[:id])
+  def show
+    @user = User.find_by(id: params[:id])
 
-        if @user.nil?
-          redirect_to users_path
-          return
-        end
-    end
+    return unless @user.nil?
 
-    def posts
-      @user_posts = @user.posts
-    end
+    redirect_to users_path
+    nil
+  end
 
-    def show_user_post
-        
-    end
+  def posts
+    @user_posts = @user.posts
+  end
 
-    private
+  def show_user_post; end
 
-    def set_user
-      @user = User.find(params[:id])
-      rescue ActiveRecord::RecordNotFound
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    flash[:error] = 'User not found'
+    redirect_to users_path
+  end
+
+  def set_post
+    if @user.nil?
       flash[:error] = 'User not found'
-      redirect_to users_path
-    end
+      @user = nil
 
-    def set_post
-        if @user.nil?
-          flash[:error] = 'User not found'
-          # redirect_to user_path
-          @user = nil
+    else
+      @post = @user.posts.find_by(id: params[:post_id])
 
-        else
-          @post = @user.posts.find_by(id: params[:post_id])
-    
-          if @post.nil?
-            flash[:error] = 'Post not found'
-            #redirect_to user_posts_path(@user)
-            @post = nil
-          end
-        end
+      if @post.nil?
+        flash[:error] = 'Post not found'
+        # redirect_to user_posts_path(@user)
+        @post = nil
+      end
     end
+  end
 end
