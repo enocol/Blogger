f9bacb75e5921434cdaeb15fe801baa0fc560708
enocol <eno_col@yahoo.com>
2024-01-24 08:01:32 +0000

    Update files

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'author_id'
   has_many :comments, dependent: :destroy, counter_cache: true, foreign_key: 'post_id'
-  # has_many :likes, foreign_key: 'post_id', dependent: :destroy, counter_cache: true
-  has_many :likes, dependent: :destroy, counter_cache: true
+  has_many :likes, foreign_key: 'post_id', dependent: :destroy, counter_cache: true
+  # has_many :likes, dependent: :destroy, counter_cache: true
   validates :title, presence: true, length: { maximum: 250 }
   validates :comments_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :likes_counter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,53 +10,53 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 20_240_117_131_428) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_17_131428) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'comments', force: :cascade do |t|
-    t.integer 'user_id'
-    t.integer 'post_id'
-    t.text 'text'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['post_id'], name: 'index_comments_on_post_id'
-    t.index ['user_id'], name: 'index_comments_on_user_id'
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
+    t.text "text"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table 'likes', force: :cascade do |t|
-    t.integer 'user_id', null: false
-    t.integer 'post_id', null: false
-    t.datetime 'created_at', default: -> { 'CURRENT_TIMESTAMP' }, null: false
-    t.datetime 'updated_at', default: -> { 'CURRENT_TIMESTAMP' }, null: false
-    t.index ['post_id'], name: 'index_likes_on_post_id'
-    t.index ['user_id'], name: 'index_likes_on_user_id'
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
-  create_table 'posts', force: :cascade do |t|
-    t.bigint 'author_id'
-    t.string 'title', limit: 250, null: false
-    t.text 'text'
-    t.integer 'comments_counter', default: 0
-    t.integer 'likes_counter', default: 0
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['author_id'], name: 'index_posts_on_author_id'
+  create_table "posts", force: :cascade do |t|
+    t.bigint "author_id"
+    t.string "title", limit: 250, null: false
+    t.text "text"
+    t.integer "comments_counter", default: 0
+    t.integer "likes_counter", default: 0
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.text 'bio'
-    t.string 'photo'
-    t.integer 'posts_counter', default: 0
-    t.integer 'role', default: 1
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.text "bio"
+    t.string "photo"
+    t.integer "posts_counter", default: 0
+    t.integer "role", default: 1
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'comments', 'posts'
-  add_foreign_key 'comments', 'users'
-  add_foreign_key 'likes', 'posts'
-  add_foreign_key 'likes', 'users'
-  add_foreign_key 'posts', 'users', column: 'author_id'
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
+  add_foreign_key "posts", "users", column: "author_id"
 end
